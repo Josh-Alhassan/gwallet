003f2b9c20bfa3571a30f7b4e52292c232894c07
Alhassan Joshua Abel <41088053+Saint-Josh@users.noreply.github.com>
2022-01-14 13:06:20 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,6 +7,27 @@ btnNavEl.addEventListener("click", function () {
   headerEL.classList.toggle("nav-open");
 });
 
+
+// ///////////////////////////////////
+// REVEAL
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for(let i = 0; i < reveals.length; i++ ) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
